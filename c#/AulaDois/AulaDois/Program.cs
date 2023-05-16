e33df81ffe22ae284b3314580e74c4ca0364871a
Ivan Viana <82772539+ivaraujo@users.noreply.github.com>
2023-05-16 15:59:07 -0300

    Aula de Variaveis

--- c#/AulaDois/AulaDois/Program.cs
+++ c#/AulaDois/AulaDois/Program.cs
@@ -4,7 +4,17 @@
     {
         static void Main(string[] args)
         {
+            //int - armazena valores númericos de interios
+            int numero = 10;
+            //float - armazena valores númericos quebrados
+            float numero2 = 2.5f;
+            //string - armazena texto
+            string nome = "Ivan";
+            //char - armazena uma letra
+            char caracter = 'I';
 
+            Console.WriteLine(nome);
+            Console.WriteLine(numero);
         }
     }
 }
